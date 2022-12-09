5e8ec81fa1507a6a6fcd782c10ce6e62ed464249
PsyduckLiu <25915043@qq.com>
2022-12-09 13:15:36 +0800

    distributed16

--- ConsensusNode/util/util.go
+++ ConsensusNode/util/util.go
@@ -68,11 +68,8 @@ func WriteResult(output string) {
 }
 
 func Encode(input []byte) ([]byte, error) {
-	// 创建一个新的 byte 输出流
 	var buf bytes.Buffer
-	// 创建一个新的 gzip 输出流
 	gzipWriter := gzip.NewWriter(&buf)
-	// 将 input byte 数组写入到此输出流中
 	_, err := gzipWriter.Write(input)
 	if err != nil {
 		_ = gzipWriter.Close()
@@ -81,24 +78,24 @@ func Encode(input []byte) ([]byte, error) {
 	if err := gzipWriter.Close(); err != nil {
 		return nil, err
 	}
-	// 返回压缩后的 bytes 数组
+
 	return buf.Bytes(), nil
 }
 
 func Decode(input []byte) ([]byte, error) {
-	// 创建一个新的 gzip.Reader
 	bytesReader := bytes.NewReader(input)
 	gzipReader, err := gzip.NewReader(bytesReader)
 	if err != nil {
 		return nil, err
 	}
-	defer func() {
-		// defer 中关闭 gzipReader
-		_ = gzipReader.Close()
-	}()
+	// defer func() {
+	// 	_ = gzipReader.Close()
+	// }()
 	buf := new(bytes.Buffer)
-	// 从 Reader 中读取出数据
 	if _, err := buf.ReadFrom(gzipReader); err != nil {
+		return nil, err
+	}
+	if err := gzipReader.Close(); err != nil {
 		return nil, err
 	}
 	return buf.Bytes(), nil
